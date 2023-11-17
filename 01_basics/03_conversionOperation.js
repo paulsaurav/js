490d2274a81944777c2775c50396ad9a7cdb4b0a
Saurav Paul <sauravpaul03@gmail.com>
2023-11-17 23:25:41 +0530

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -9,7 +9,7 @@ let score = "50anskd"
 let isLoggedIn = 1
 
 let newIsLoggedIn = Boolean(isLoggedIn)
-console.log(newIsLoggedIn);
+// console.log(newIsLoggedIn);
 
 /* 
 1= true
@@ -19,3 +19,42 @@ console.log(newIsLoggedIn);
 "anything" = true
 
 */
+
+let aNumber = 50
+let newNumber = String(aNumber)
+// console.log(newNumber)
+// console.log(typeof newNumber);
+
+
+
+// ************************OPERATIONS************************
+
+let value = 10
+let negValue = -value
+// console.log(negValue);
+
+//Mathematical Operations
+// console.log(5+5)
+// console.log(5*5)
+// console.log(5-5)
+// console.log(5**2)
+// console.log(5/5)
+// console.log(3%2)
+
+
+let str1 = "Hello "
+let str2 = "Saurav"
+let str3 = str1 + str2
+// console.log(str3); 
+
+// console.log(1+1)
+// console.log("1"+1)
+// console.log("1"+1+1)
+// console.log(1+1+"1")
+
+// console.log(+true)
+// console.log(+"")
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
