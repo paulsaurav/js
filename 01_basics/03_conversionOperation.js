5e071829b6cce0407a38b9d5e37205bf51f9c134
Saurav Paul <sauravpaul03@gmail.com>
2023-11-17 22:46:19 +0530

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score = "50anskd"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+// let newScore = Number(score)
+// console.log(newScore);
+
+let isLoggedIn = 1
+
+let newIsLoggedIn = Boolean(isLoggedIn)
+console.log(newIsLoggedIn);
+
+/* 
+1= true
+0= false
+1> = true
+"" = false
+"anything" = true
+
+*/
