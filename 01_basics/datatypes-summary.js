1c7add6c0fe9c429983103ca3a4a86a71b5f0b89
Saurav Paul <sauravpaul03@gmail.com>
2023-11-19 12:45:00 +0530

    datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,39 @@
+// Premitive Datatypes
+// 1. Number
+// 2. String
+// 3. Boolean
+// 4. BigInt
+// 5. Null
+// 6. Undefined
+// 7. Symbol
+
+
+//Reference Datatypes
+// 1. Object
+// 2. Function
+// 3. Array
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+
+// console.log(id === anotherId)
+
+
+//Array
+const heros = ["Batman", "Superman", "Spiderman"]
+
+//Object
+let myObj = {
+    name : "Saurav",
+    age : 25,
+    isLoggedIn : false
+}
+
+//Function
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+console.log(myFunction);
