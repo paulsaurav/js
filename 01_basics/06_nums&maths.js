356056657709c9c014aa1fab5add537a26d541be
Saurav Paul <sauravpaul03@gmail.com>
2023-11-22 12:14:27 +0530

    Maths

---
+++ 01_basics/06_nums&maths.js
@@ -0,0 +1,31 @@
+const score = 500
+// console.log(score)
+
+const balance = new Number(300)
+// console.log(balance)
+
+// console.log(typeof balance.toString());
+// console.log(balance.toFixed(2));
+
+
+const anotherNumber = 22.636499
+// console.log(anotherNumber.toPrecision(4))
+
+const newNumber = 1000000000
+// console.log(newNumber.toLocaleString('en-IN'));
+
+//==========================MATHS===========================
+
+// console.log(Math.abs(-100));
+// console.log(Math.round(2.4));
+// console.log(Math.ceil(2.0001));
+// console.log(Math.floor(2.9));
+// console.log(Math.pow(3, 2));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
